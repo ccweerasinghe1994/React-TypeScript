a931450b182509da0be8716b08fa791d4376711a
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-01-11 18:03:35 +0530

    adding redux and reducer

---
+++ ts-redux/src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,37 @@
+interface RepositoriesState{
+    error:string|null;
+    loading:boolean;
+    data:string[]
+}
+
+export const reducer = (state:RepositoriesState,action:any):RepositoriesState=>{
+
+
+    switch (action.type) {
+        
+            
+            
+    case "search_repositories":
+        return{
+            loading:true,
+            error:null,
+            data:[]
+        }
+    case "search_repositories_success":
+          return{
+            loading:false,
+            error:null,
+            data:action.payload
+        }
+    case "search_repositories_error": 
+           return{
+            loading:false,
+            error:action.payload,
+            data:[]
+        }
+        default:
+            return state
+    }
+
+}
+
