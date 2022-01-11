b8ad9fff17aaaf2749e6fab6dc084a64af2f5e87
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-01-11 10:59:58 +0530

    adding styled components and basic form to the application

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,10 +1,23 @@
 import ReactDOM from "react-dom";
+import GuestList from "./state/guest-list";
+import {createGlobalStyle} from 'styled-components';
 
+const GlobalStyles = createGlobalStyle`
+html{
+    font-family: 'Bebas Neue', cursive;
+}
+
+body{
+    background-image: linear-gradient(to bottom right,rgba(241, 113, 9, 0.1),rgba(219, 80, 16, 0.1));
+}
+
+`
 
 const App =()=>(
-    <div>
-        Hello World
-    </div>
+    <>
+    <GlobalStyles/>
+        <GuestList/>
+    </>
 )
 
 ReactDOM.render(<App/>,document.querySelector('#root'));
---
+++ my-app/src/state/guest-list.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const CustomButton = styled.button`
+
+$primary-color:rgba(245, 82, 7, 0.5);
+border: none;
+background-color: aliceblue;
+border:1px solid  #0e9dd62f;
+color: #1615159d;
+cursor: pointer;
+transition: all 0.3s ease-out;
+padding:.5rem 1.5rem;
+display: inline-block;
+border-radius: 3px;
+font-size: 1.5rem;
+text-align: center;
+text-transform: capitalize;
+font-family: inherit;
+font-weight: 400;
+letter-spacing: 1px;
+&:hover{
+    transform: scale(1.03) translateY(-1px);
+    box-shadow: 0px 3px 10px rgba(245, 82, 7, 0.5);
+}
+&:active{
+    transform: scale(1) translateY(0px);
+    box-shadow: 0px 3px 5px rgba(245, 82, 7, 0.5);
+}
+
+`
+
+const Wrapper = styled.div`
+border: 10px solid rgba(245, 82, 7, 0.5);
+padding: 2rem;
+margin: 5rem;
+border-radius: 13px;
+
+`
+const Header = styled.h3`
+text-align: center;
+font-size: 4rem;
+font-weight: 400;
+letter-spacing: 3px;
+color: #1615159d;
+`
+
+const CustomInput = styled.input`
+
+    display: block;
+    padding: 0.5rem 2rem;
+    border: 1px solid orange;
+    font-family: inherit;
+    font-size: 1.5rem;
+    margin-bottom:2rem;
+    
+
+
+`
+const GuestList:React.FC=()=>{
+   
+    const [name,setName] =  useState("");
+    return <Wrapper>
+        <Header>Guest List</Header>
+        <CustomInput placeholder='enter a name' value={name} onChange={(e)=>setName(e.target.value)} type="text" />
+        <CustomButton>Add Guest</CustomButton>
+    </Wrapper>
+    
+}
+
+export default GuestList;
