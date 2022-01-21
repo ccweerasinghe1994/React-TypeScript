6aab151fdac961d1c9bf05b5ab6e5503987c4ff7
chamara <chamara.weerasinghe@my.sliit.lk>
2022-01-21 14:59:57 +0530

    adding in browser transpiling

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 const App: React.FC = () => {
+  const ref = useRef<any>();
   const [input, SetInput] = useState("");
   const [code, setCode] = useState("");
 
-  const handleSubmit = () => {
-    console.log(input);
+  const startService = async () => {
+    await esbuild.initialize({
+      wasmURL: "/esbuild.wasm",
+    });
+    ref.current = await esbuild.build;
+
+    // const transform = await esbuild.transform(
+    //   'import ReactDOM from "react-dom";'
+    // );
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const handleSubmit = async () => {
+    if (!ref.current) return;
+    console.log(ref.current);
+
+    const data = await ref.current({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    console.log(data.outputFiles[0].text);
+
+    setCode(data.outputFiles[0].text);
   };
   return (
     <>
