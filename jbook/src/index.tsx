14681c211bcce32426280c74a21c2530a679fa06
chamara <chamara.weerasinghe@my.sliit.lk>
2022-01-21 12:45:06 +0530

    adding basci ui layout

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-const App = () => <h1>Hi</h1>;
+const App: React.FC = () => {
+  const [input, SetInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleSubmit = () => {
+    console.log(input);
+  };
+  return (
+    <>
+      <textarea
+        value={input}
+        onChange={(e) => SetInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={handleSubmit}>submit</button>
+      </div>
+      <pre>{code}</pre>
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
