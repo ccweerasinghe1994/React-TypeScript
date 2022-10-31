6994e724bec2bf0cb382b86abbc39d204d5664e9
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-10-31 06:25:29 +0530

    adding simple form

--- jbook-latest/src/App.tsx
+++ jbook-latest/src/App.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div>Hello World</div>;
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value);
+  };
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(input);
+  };
+  return (
+    <>
+      <textarea
+        value={input}
+        onChange={handleChange}
+        name=''
+        id=''
+        cols={30}
+        rows={10}
+      ></textarea>
+      <div>
+        <button onClick={handleClick} type='button'>
+          Submit
+        </button>
+        <pre></pre>
+      </div>
+    </>
+  );
 }
 
 export default App;
