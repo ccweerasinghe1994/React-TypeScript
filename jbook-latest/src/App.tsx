c5c4ac69e7c6b0426c0d8a4aa495443404087371
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-11-02 07:22:54 +0530

    update

--- jbook-latest/src/App.tsx
+++ jbook-latest/src/App.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [input, setInput] = useState<string>('');
   const [code, setCode] = useState<string>('');
+  const startService = async () => {
+    const service = await esbuild.initialize({
+      worker: true,
+      wasmURL: './esbuild.wasm',
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(event.target.value);
   };
